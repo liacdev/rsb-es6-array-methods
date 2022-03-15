b9b7fe4808261850e310be7ddf79cb7c186dd1ee
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-03-15 13:05:09 -0500

    functoin comopnent made

---
+++ src/Components/array-methods.js
@@ -0,0 +1,10 @@
+function ArrayMethods() {
+  return (
+    <div>
+      <h2>React Array Methods</h2>
+      <p>One of the most useful in React is the .map() array method.</p>
+    </div>
+  );
+}
+
+export default ArrayMethods;
